f7229c0fbac3d5879a22bcf591a87705c3903939
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-04 20:52:12 +0300

    Configures the url of the project & apps

--- gmc_test/urls.py
+++ gmc_test/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import include
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
+tokens_url = [
+    path('', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('verify/', TokenVerifyView.as_view()),
+]
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', include(tokens_url)),
     path('', include('library.urls')),
 ]
--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import url
-
-from . import views
+from django.urls import path
+from django.views.generic import TemplateView
 
 
 app_name = 'library'
 urlpatterns = [
-    url(r'^index?', views.index, name='index'),
+
 ]
