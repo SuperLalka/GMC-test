60078c3c91e0c9473e1dcdbbbd40717c2ce24755
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-04 20:58:08 +0300

    Adds admin panel & shell file to launch the project

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import (
+    Author, Book, Category, User
+)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn', 'author', 'category')
+    list_filter = ('author', 'category')
+    search_fields = ['title', 'isbn']
+    list_per_page = 50
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'supercategory')
+    search_fields = ['name']
+    list_per_page = 50
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    search_fields = ['last_name']
+    list_per_page = 50
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'user_group', 'is_active', 'is_staff')
+    list_filter = ('user_group', 'is_staff')
+    list_per_page = 20
