d37c9655ed964acb84b834530578837d5801ecb3
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-06 17:02:13 +0300

    Adds API for working with the server & for authorization

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from django.views.generic import TemplateView
+from django.conf.urls import url
+from django.urls import include
+
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
+routerAPI = DefaultRouter()
+routerAPI.register(r'books', views.BookViewSet, basename='books')
+routerAPI.register(r'categories', views.CategoryViewSet, basename='categories')
 
 
 app_name = 'library'
 urlpatterns = [
-
+    url(r'^api/', include(routerAPI.urls)),
 ]
