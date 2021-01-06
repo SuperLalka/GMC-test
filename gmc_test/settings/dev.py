5dacf634b979c8c6efccdb554b265218460f84de
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-06 17:04:05 +0300

    Connects CORS to Django to interact with a Vue app

--- gmc_test/settings/dev.py
+++ gmc_test/settings/dev.py
@@ -3,6 +3,15 @@ from .settings import *
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'http://localhost:8080',
+)
+
 MEDIA_URL = '/dmedia/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
