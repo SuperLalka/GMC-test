44b0ce7a88ca3cf3e543ae1c5c2345a08b92d2c6
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-06 23:21:42 +0300

    Configures the vuex store to store user's auth status

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,24 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
   state: {
+    is_Authorize: false
   },
   mutations: {
+    CHANGE_AUTH_STATUS: (state) => {
+      state.is_Authorize = !state.is_Authorize;
+    }
   },
   actions: {
+    TOGGLE_AUTH_STATUS({commit}) {
+      commit('CHANGE_AUTH_STATUS')
+    }
   },
-  modules: {
-  }
+  getters: {
+    IS_AUTH_STATE(state) {
+      return state.is_Authorize;
+    }
+  },
+  plugins: [createPersistedState()],
 })
